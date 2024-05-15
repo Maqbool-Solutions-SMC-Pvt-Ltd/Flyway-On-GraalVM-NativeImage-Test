fe8431db1307bf6d6af2d76b5ab8e6bbef27de84
Abid Maqbool <abid.maqbool55@gmail.com>
2024-05-15 13:11:11 +0500

    Add missing files;

---
+++ src/main/resources/db/migrations/V1_0_1__Create_Table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS tbl_message (
+    msg VARCHAR(100),
+    sendby VARCHAR(100)
+);
---
+++ src/main/resources/db/migrations/V1_0_2__Insert_Data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tbl_message (msg, sendby) VALUES
+(NOW(), 'Admin1'),
+(NOW(), 'Admin2'),
+(NOW(), 'Admin3'),
+(NOW(), 'Admin4'),
+(NOW(), 'Admin5');
